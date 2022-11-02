4e29b5fa0fb50f0a2fef030ae9ce539a26dbfb89
YeseseniaGraciela <99694794+YeseseniaGraciela@users.noreply.github.com>
2022-11-01 19:52:39 -0500

    creación de la base
    se crea la base de la calculadora

---
+++ calculadora.cs
@@ -0,0 +1,38 @@
+using System;
+
+public class Class1
+{
+	private int num1;
+	private int num2;
+
+	public Numero()
+	{
+	}
+
+    public global::System.Int32 Num1 { get => num1; set => num1 = value; }
+    public global::System.Int32 Num2 { get => num2; set => num2 = value; }
+    
+    #Operación sumas
+    
+    
+    
+    
+    #Operación restar
+    
+    
+    
+    
+    
+    #Operación multiplicar
+    
+    
+    
+    
+    
+    #Operación divivir
+    
+    
+    
+    
+
+}
